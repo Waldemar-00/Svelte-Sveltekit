2f5aa87c8032bfc440785fe6fd1eda13db870a29
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-01 16:52:51 +0300

    Was used browser from '/invironment'

--- src/routes/searcher/+page.svelte
+++ src/routes/searcher/+page.svelte
@@ -1,17 +1,36 @@
 <script>
 	// @ts-nocheck
-	import { onMount } from 'svelte';
-	import { page } from '$app/stores';
+	// import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { page, navigating, updated } from '$app/stores';
+	// console.log($navigating);
+	// console.log($updated);
+
 	export let data;
 	let header = 'Some';
 	//! Why the code below don't return the headers?
-	onMount(async () => {
+	// onMount(async () => {
+	// 	const response = await fetch(window.location.href);
+	// 	header = await response.headers.get('x-custom-header');
+	// });
+	async function inBrowser() {
 		const response = await fetch(window.location.href);
 		header = await response.headers.get('x-custom-header');
-	});
+	}
+	$: if (browser) {
+		inBrowser();
+	}
 </script>
 
-<svelte:head><title>searcher</title></svelte:head>
+<svelte:head
+	><title>searcher</title>
+	<meta
+		name="description"
+		content="Mary's simple recipe for maple bacon donuts
+    makes a sticky, sweet treat with just a hint
+    of salt that you'll keep coming back for."
+	/></svelte:head
+>
 <!--for availability and SEO on each page!-->
 <h1>{header}</h1>
 <form action="/searcher">
