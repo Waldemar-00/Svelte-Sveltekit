62ea5f1a37520ddec520a50d56182cfe7d21dc8f
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-11 19:59:53 +0300

    svelte:document and visibilityState(change), and fullscreenElement(change). Also use:action.

--- src/routes/special/document/+page.svelte
+++ src/routes/special/document/+page.svelte
@@ -1,11 +1,35 @@
 <script>
 	// @ts-nocheck
-	let visibilityState = false;
+	let fullscreenElement;
+	let visibilityState;
 	function handleContextMenu(e) {
 		e.preventDefault();
 		alert('Context Menu is not opened after right click!');
 	}
+	let count = 0;
+	function foo(node, { count }) {
+		alert('use:action');
+		return {
+			update({ count }) {
+				if (count > 7) document.body.style.backgroundColor = 'lightblue';
+			},
+			destroy() {
+				alert('Destroy');
+				node.body.style.backgroundColor = '#fff';
+			}
+		};
+	}
 </script>
 
 <h1>svelte:document</h1>
-<svelte:document bind:visibilityState on:contextmenu={handleContextMenu} />
+<svelte:document
+	bind:fullscreenElement
+	bind:visibilityState
+	on:contextmenu={handleContextMenu}
+	use:foo={{ count }}
+/>
+<button on:click={() => console.log(fullscreenElement)}>fullscreenElement</button>
+<button on:click={() => console.log(visibilityState)}>visibilitySate</button>
+<button on:click={() => count++}>Change count and see via use:action</button>
+<h2>{count}</h2>
+<!--USE:ACTION!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!-->
