6c0a1c774382b2a9e6a7ab25b331784151935035
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-13 21:19:58 +0300

    Store: derived.

--- src/routes/stores/derived/+page.svelte
+++ src/routes/stores/derived/+page.svelte
@@ -1,1 +1,21 @@
+<script>
+	// @ts-nocheck
+
+	import { derived, writable } from 'svelte/store';
+	const a = writable(200);
+	const b = writable(300);
+	const summed = derived([a, b], ([$a, $b]) => $a + $b);
+</script>
+
 <h1>Derived</h1>
+<h2>{$summed}</h2>
+<button
+	on:click={() => {
+		a.update((a) => a + 100);
+	}}>increase A</button
+>
+<button
+	on:click={() => {
+		b.update((b) => b + 200);
+	}}>increase B</button
+>
--- src/routes/stores/writable/+page.svelte
+++ src/routes/stores/writable/+page.svelte
@@ -6,16 +6,23 @@
 		console.log('SUBSCRIBE'); //first
 		return () => console.log('UNSUBSCRIBE');
 	});
+	const store_1 = writable(250);
 	const unsubscribe = store.subscribe((store) => {
 		console.log(store.a); //second
 	});
 	unsubscribe();
 	store.set({ ...store, a: 2 });
 	store.update((a) => {
-		const res = a.a * 10;
+		const res = a.a * 100;
 		return { ...a, a: res };
 	});
 </script>
 
 <h1>Writable</h1>
 <h2>{$store.a}</h2>
+<button
+	on:click={() => {
+		const res = $store.a + $store_1;
+		store.set({ ...store, a: res });
+	}}>increasing</button
+>
