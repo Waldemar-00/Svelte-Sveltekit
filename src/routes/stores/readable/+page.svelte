394046969d1e33f0a3d6a6d55fef4050bcccafe1
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-13 20:34:22 +0300

    Stores: writable, readable.

--- src/routes/stores/readable/+page.svelte
+++ src/routes/stores/readable/+page.svelte
@@ -1,1 +1,22 @@
+<script>
+	// @ts-nocheck
+
+	import { readable } from 'svelte/store';
+	const time = readable(0, (set) => {
+		set(`${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`);
+		const interval = setInterval(() => {
+			set(`${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`);
+		}, 1000);
+		return () => clearInterval(interval);
+	});
+	const clock = readable('tick', (set, update) => {
+		const interval = setInterval(() => {
+			update((sound) => (sound === 'tick' ? 'tock' : 'tick'));
+		}, 1000);
+		return () => clearInterval(interval);
+	});
+</script>
+
 <h1>Readable</h1>
+
+<h2>Time: {$time} {$clock}</h2>
--- src/routes/stores/writable/+page.svelte
+++ src/routes/stores/writable/+page.svelte
@@ -1,14 +1,21 @@
 <script>
+	// @ts-nocheck
+
 	import { writable } from 'svelte/store';
-	const store = writable({ a: 1 }, () => {
-		console.log('SUBSCRIBE');
+	const store = writable({ a: 1 }, (set) => {
+		console.log('SUBSCRIBE'); //first
 		return () => console.log('UNSUBSCRIBE');
 	});
-	store.set({ ...store, a: 2 });
 	const unsubscribe = store.subscribe((store) => {
-		console.log(store.a);
+		console.log(store.a); //second
 	});
 	unsubscribe();
+	store.set({ ...store, a: 2 });
+	store.update((a) => {
+		const res = a.a * 10;
+		return { ...a, a: res };
+	});
 </script>
 
 <h1>Writable</h1>
+<h2>{$store.a}</h2>
