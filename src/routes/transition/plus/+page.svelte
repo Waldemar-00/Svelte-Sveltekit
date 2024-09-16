f72a4bb5a1b8be9023f09461d29b327bf711ae83
ULADZIMIR MARTYNCHYK <01myit@gmail.com>
2024-09-16 21:13:46 +0300

    Svelte transition: slide with quintOut)).

--- src/routes/transition/plus/+page.svelte
+++ src/routes/transition/plus/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	import { blur } from 'svelte/transition';
-	import { cubicOut, linear } from 'svelte/easing';
+	import { blur, slide } from 'svelte/transition';
+	import { cubicOut, linear, quintOut } from 'svelte/easing';
 </script>
 
 <h1
@@ -9,5 +9,18 @@
 >
 	Blur
 </h1>
+<ul in:slide={{ duration: 1000, axis: 'y', easing: quintOut }}>
+	<li><h2>Hello</h2></li>
+	<li><h2>Hello Hello</h2></li>
+	<li><h2>Hello Hello Hello</h2></li>
+	<li><h2>Hello, Great SLIDE!</h2></li>
+</ul>
+
 <!--default amount in px, you can use 'rem' -->
 <a href="/transition">Back to transition</a>
+
+<style>
+	ul {
+		list-style: none;
+	}
+</style>
